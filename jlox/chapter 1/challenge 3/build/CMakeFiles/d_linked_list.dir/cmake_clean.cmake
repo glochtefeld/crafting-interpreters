19650d177f0596584903596aa588de1cd5c7c70a
Gavin <gavin.loch@gmail.com>
2022-05-01 14:49:08 -0500

    starting section ii

---
+++ jlox/chapter 1/challenge 3/build/CMakeFiles/d_linked_list.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/d_linked_list.dir/src/double-link-list.c.o"
+  "libd_linked_list.a"
+  "libd_linked_list.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/d_linked_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ jlox/chapter 1/challenge 3/build/CMakeFiles/dlinkedlist_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dlinkedlist_test.dir/src/main.c.o"
+  "dlinkedlist_test"
+  "dlinkedlist_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dlinkedlist_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
