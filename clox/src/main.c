021e0521c01e9ac80f048d0b4a45e44281625e4e
Gavin Lochtefeld <gavin.loch@gmail.com>
2022-05-05 12:54:56 -0500

    initial

--- clox/src/main.c
+++ clox/src/main.c
@@ -7,26 +7,130 @@ int main(int argc, const char* argv[]) {
     initVM();
     Chunk chunk;
     initChunk(&chunk);
-    int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
 
-    constant = addConstant(&chunk, 3.4);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
+    int constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
 
-    writeChunk(&chunk, OP_ADD, 123);
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
 
-    constant = addConstant(&chunk, 5.6);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
+    writeChunk(&chunk, OP_MULTIPLY, 1);
 
-    writeChunk(&chunk, OP_DIVIDE, 123);
-    writeChunk(&chunk, OP_NEGATE, 123);
-    writeChunk(&chunk, OP_RETURN, 123);
+    constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_ADD, 1);
+
+    constant = addConstant(&chunk, 4);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    constant = addConstant(&chunk, 5);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_NEGATE, 1);
+
+    writeChunk(&chunk, OP_DIVIDE, 1);
+
+    writeChunk(&chunk, OP_SUBTRACT, 1);
+
+    
+    writeChunk(&chunk, OP_RETURN, 1);
+
     disassembleChunk(&chunk, "Test chunk");
     interpret(&chunk);
     freeVM();
     freeChunk(&chunk);
     return 0;
 }
+
+/* Challenge 1:
+ *  1 * 2 + 3
+ *  int constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_MULTIPLY, 1);
+
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_ADD, 1);
+
+ *  1 + 2 * 3
+ *  int constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_MULTIPLY, 1);
+
+    constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_ADD, 1);
+
+ *  3 - 2 - 1
+ *  int constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_SUBTRACT, 1);
+
+    constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_SUBTRACT, 1);
+
+ *  1 + 2 * 3 - 4 / 5
+ *  int constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_MULTIPLY, 1);
+
+    constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_ADD, 1);
+
+    constant = addConstant(&chunk, 4);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    constant = addConstant(&chunk, 5);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_NEGATE, 1);
+
+    writeChunk(&chunk, OP_DIVIDE, 1);
+
+    writeChunk(&chunk, OP_SUBTRACT, 1);
+
+* Challenge 2:
+* 4 - 3 * -2 Without OP_NEGATE: 4 - (3 * (0 - 2))
+*/
